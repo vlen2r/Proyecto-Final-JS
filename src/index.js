afde66b65ae20e6aa33ec1461c897d399ce02f2f
Vlen2R <71619929+vlen2r@users.noreply.github.com>
2021-07-01 21:16:44 -0300

    Updates nuevos

--- src/index.js
+++ src/index.js
@@ -2,9 +2,22 @@ import "./styles.css";
 import * as Usuario_JSON from "./js/usuario-provider";
 import { Usuario } from "./classes/usuario.class";
 
+// Botones
+const btnCreateUser = document.querySelector('#create_user');
+
+// Form}
+const formCreateUser = document.querySelector('#form_create_user');
+
+
 Usuario_JSON.getUsuario(1)
     .then(msg => { console.log(msg) })
     .catch(msg => { console.error(msg) });
 
 const user1 = new Usuario(1, 'valentin@test.com', 'Valentín', 'Test');
 Usuario_JSON.createUsuario(user1).then(console.log);
+
+btnCreateUser.addEventListener('click', (event) => {
+    btnCreateUser.disabled = true;
+    console.log(formCreateUser.children.length);
+
+});
